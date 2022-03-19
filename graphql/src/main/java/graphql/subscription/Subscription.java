10764c911bcb120386078a4136b7b58430a76c1d
Erkan <erkan000@hotmail.com>
2022-03-19 20:52:38 +0300

    graphql ksqldb

--- graphql/src/main/java/graphql/subscription/Subscription.java
+++ graphql/src/main/java/graphql/subscription/Subscription.java
@@ -1,52 +1,22 @@
 package graphql.subscription;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.time.Duration;
+import java.util.UUID;
 
 import org.reactivestreams.Publisher;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import graphql.dto.Person;
 import graphql.kickstart.tools.GraphQLSubscriptionResolver;
-import io.confluent.ksql.api.client.Client;
-import io.confluent.ksql.api.client.ClientOptions;
+import reactor.core.publisher.Flux;
 
 @Component
 public class Subscription implements GraphQLSubscriptionResolver {
-
-	public Publisher<Person> streamPersonEvent() throws MalformedURLException {
-
-		URL endpoint = new URL("http://localhost:8088");
-		final ClientOptions options = ClientOptions.create()
-				.setHost(endpoint.getHost())
-				.setPort(endpoint.getPort());
-
-		logger.info("Subscribe request arrived");
-
-		final ReactorClient reactorClient = ReactorClient.from(Client.create(options));
-
-		return reactorClient
-				.streamQuery("SELECT * from ServiceCategorySourceTAble2 emit changes;")
-				.map(row -> {
-					return new Person(row.getValue(1).toString(), 3, row.getValue(2).toString());
-				});
-		
-		//		Client client = Client.create(options);
-		//		CompletableFuture<StreamedQueryResult> stream = client.streamQuery("SELECT * from LeftyProviderServiceViewTAble emit changes;");
-		//		Publisher<Row> result = FlowAdapters.toFlowPublisher(stream.get());		
-		//		stream.thenAccept(streamedQueryResult -> {
-		//			System.out.println("Query has started. Query ID: " + streamedQueryResult.queryID());
-		//			RowSubscriber subscriber = new RowSubscriber();
-		//			streamedQueryResult.subscribe(subscriber);
-		//		}).exceptionally(e -> {
-		//			System.out.println("Request failed: " + e);
-		//			return null;
-		//		});
-	}
-
 	
-	private Logger logger = LoggerFactory.getLogger(Subscription.class);
+	public Publisher<Person> streamPersonEvent() {
+		
+		return Flux.interval(Duration.ofSeconds(1))
+                .map(num -> new Person(UUID.randomUUID().toString(), 5, "email"));
+	}
 
 }
